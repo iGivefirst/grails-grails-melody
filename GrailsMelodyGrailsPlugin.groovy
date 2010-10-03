7e4f63a0cced2971fd39b463f2e8d68c8e2733bf
evernat <evernat@832c1c66-4827-0410-8465-ccb17913cd09>
2010-10-03 22:24:25 +0000

    Version 0.5 including JavaMelody 1.20.0
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-grails-melody/trunk@65048 832c1c66-4827-0410-8465-ccb17913cd09

--- GrailsMelodyGrailsPlugin.groovy
+++ GrailsMelodyGrailsPlugin.groovy
@@ -9,7 +9,7 @@ class GrailsMelodyGrailsPlugin {
     // the plugin version
     def version = "0.5"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.1.1 > *"
+    def grailsVersion = "1.2.4 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
