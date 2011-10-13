bca676b2a96be3fda59a1d585bfbea5defd6e659
evernat <evernat@832c1c66-4827-0410-8465-ccb17913cd09>
2011-10-13 05:54:31 +0000

    javamelody 1.32.0
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-grails-melody/trunk@75506 832c1c66-4827-0410-8465-ccb17913cd09

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,11 +8,11 @@ grails.project.dependency.resolution = {
 		grailsPlugins()
 		grailsHome()
 		mavenCentral()
-		mavenRepo "http://maven.glassfish.org/content/groups/public/" //repo for javamelody and jrobin
+		mavenRepo "http://maven.glassfish.org/content/groups/public/" //repo for jrobin
 	}
 	dependencies {
 		// change the javamelody version here to upgrade
-		compile "net.bull.javamelody:javamelody-core:1.31.0"
+		compile "net.bull.javamelody:javamelody-core:1.32.0"
 		compile ("com.lowagie:itext:2.1.7") {excludes "bcmail-jdk14", "bcprov-jdk14", "bctsp-jdk14"}
 		compile "org.jrobin:jrobin:1.5.9"
 	}
