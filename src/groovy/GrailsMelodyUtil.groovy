0815829639e234d3741fad159e4edfb8fffbf258
evernat <evernat@832c1c66-4827-0410-8465-ccb17913cd09>
2012-02-19 10:33:31 +0000

    fix for javamelody issue 180 http://code.google.com/p/javamelody/issues/detail?id=180
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-grails-melody/trunk@79849 832c1c66-4827-0410-8465-ccb17913cd09

--- src/groovy/GrailsMelodyUtil.groovy
+++ src/groovy/GrailsMelodyUtil.groovy
@@ -1,26 +1,15 @@
 import grails.util.GrailsUtil
-/**
- * Created by IntelliJ IDEA.
- * User: 541116
- * Date: 2009-12-10
- * Time: 21:34:31
- * To change this template use File | Settings | File Templates.
- */
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
 class GrailsMelodyUtil {
-
     static ConfigObject getGrailsMelodyConfig() {
-
+		def config = ConfigurationHolder.config
 		GroovyClassLoader classLoader = new GroovyClassLoader(GrailsMelodyUtil.getClassLoader())
-
-		def slurper = new ConfigSlurper(GrailsUtil.environment)
-		ConfigObject userConfig
 		try {
-			userConfig = slurper.parse(classLoader.loadClass('GrailsMelodyConfig'))
-		}
-		catch (e) {
+			config.merge(new ConfigSlurper(GrailsUtil.environment).parse(classLoader.loadClass('GrailsMelodyConfig')))
+		} catch (Exception e) {
 			// ignored, use defaults
 		}
-
-        return userConfig
+		return config
 	}
 }
