67557f18144cc38d600292b91bc1b7de489caa14
Chrs Love <clove@cnmconsulting.net>
2012-11-20 13:51:37 -0700

    still not working

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,5 +14,8 @@ grails.project.dependency.resolution = {
 		compile "net.bull.javamelody:javamelody-core:1.41.0"
 		compile ("com.lowagie:itext:2.1.7") {excludes "bcmail-jdk14", "bcprov-jdk14", "bctsp-jdk14"}
 		compile "org.jrobin:jrobin:1.5.9"
+    compile "commons-codec:commons-codec:1.5"
+    compile "commons-beanutils:commons-beanutils:1.8.3"
+    compile "commons-lang:commons-lang:2.6"
 	}
 }
