9d8a73ebf0960ff1d1c2b8ac21c338f45ef56f6b
Chrs Love <clove@cnmconsulting.net>
2012-11-20 13:18:33 -0700

    upgrading to 2.1.1

--- GrailsMelodyGrailsPlugin.groovy
+++ GrailsMelodyGrailsPlugin.groovy
@@ -9,13 +9,15 @@ class GrailsMelodyGrailsPlugin {
     // the plugin version
     def version = "1.18"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.4 > *"
+    def grailsVersion = "2.1.1 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     def loadAfter = ['spring-security-core', 'acegi', 'shiro']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp",
+            "test/**",
+            "src/docs/**"
     ]
 
     def author = "Liu Chao"
